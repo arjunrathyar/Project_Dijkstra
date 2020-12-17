f7f584b8a7efb1881dee11f3e1d751529cf69acb
Arjun Rathya R <arjunrathyar@am.students.amrita.edu>
2020-12-17 22:03:25 +0530

    Add files via upload

---
+++ Graph.java
@@ -0,0 +1,64 @@
+import java.util.*;
+
+public class Graph {
+	GraphNode[] node;
+	int size;
+
+	public Graph(int n) {
+		size = n;
+		node = new GraphNode[size];
+		for(int i=0; i<n; i++) {
+			node[i] = new GraphNode(i);
+		}
+	}
+	
+	public void addEdge(int from, int to, int w) {
+		node[from].adjList.add(node[to]);
+		node[to].adjList.add(node[from]);
+		node[from].weights.add(w);
+		node[to].weights.add(w);
+		}
+	
+	public void print() {
+		for(int i=0; i<this.size; i++) {
+			System.out.print(i+": ");
+			node[i].print();
+			System.out.println();
+		}
+	}
+	
+	public void reset() {
+		for(int i=0; i<size; i++) {
+			node[i].visited = false;
+			node[i].d = Integer.MAX_VALUE;
+			node[i].reset();
+		}
+	}
+	
+	public void Dijkstra(int i) {
+		reset();
+		ArrayDeque<GraphNode> q = new ArrayDeque<GraphNode>();
+		q.add(node[i]);
+		node[i].visited = true;
+		node[i].d = 0;		
+		node[i].Dijkstra(q);
+	}
+	
+	public void ShortestPath(int s) {
+		Dijkstra(s);
+		if(node[size-1].d==Integer.MAX_VALUE) {
+			System.out.println("NO PATH EXIST");
+			}
+		else {
+			System.out.println();
+			System.out.println("*****************************************************************************************************************************************");
+			System.out.println("STATION CODE                  : "+(node[s].label+1));
+			System.out.println();
+			System.out.println("DESTINATION CODE              : "+(node[size-1].label+1));
+			System.out.println();
+			System.out.println("MOST EFFICIENT PATH WILL COST : "+(node[size-1].d));
+			System.out.println("*****************************************************************************************************************************************");
+			//System.out.println("Shortest path from "+(node[s].label+1)+" to "+node[size-1].label+1+": "+node[size-1].d);
+			}
+		}
+	}
---
+++ Pair.java
@@ -0,0 +1,17 @@
+public class Pair{
+    int a;
+    int b;
+    
+    Pair(int p,int q){
+        this.a=p;
+        this.b=q;
+    }
+    
+    public int get_first() {
+    	return a;
+    }
+    
+    public int get_second() {
+    	return b;
+    }
+    }
