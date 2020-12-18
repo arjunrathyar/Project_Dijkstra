7e7eec70e1d8a77c96c6e256c98811b689bb3c15
Caster12 <57310589+Caster12@users.noreply.github.com>
2020-12-18 12:11:00 +0530

    Add files via upload

---
+++ DatasetCode/Pair.java
@@ -0,0 +1,19 @@
+
+public class Pair{
+    int a;
+    int b;
+    
+    Pair(int p,int q){
+        this.a=p;
+        this.b=q;
+    }
+    
+    public int get_first() {
+    	return a;
+    }
+    
+    public int get_second() {
+    	return b;
+    }
+    
+}
